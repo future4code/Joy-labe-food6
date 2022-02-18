ed2a85c250393d8e4a1e564046ef82779631ad21
ViniciusLussaniToigo <viniciuslussani@gmail.com>
2022-02-17 21:34:15 -0300

    Router e paginas em branco

---
+++ src/pages/Feed.js
@@ -0,0 +1,28 @@
+import React, {  useState,useEffect,useHistory } from 'react';
+
+
+const Feed = () => {
+
+//      const history = useHistory()
+
+//    const vaParaPaginaLogin = ()=>{
+//    history.push("/login");
+//     }
+
+  return (
+    <div>
+    <h1> Feed aqui  </h1>
+
+    {/* <button onClick={() => history.push('/login')} >Voltar</button> */}
+
+    </div>
+    
+    
+    
+    
+    
+  
+  );
+}
+
+export default Feed;
---
+++ src/pages/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Login = () => {
+  return (
+    <div>
+    <h1> Faça seu login! </h1>
+    
+
+
+    </div>
+    
+    
+    
+    
+    
+  
+  );
+}
+
+export default Login;
